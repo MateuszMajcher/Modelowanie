6065ca1bca1547df351b662407cbfba325598e21
Mateusz Majcher <mateuszxmajcher@gmail.com>
2016-06-05 21:03:59 +0200

    init

---
+++ Database.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+
+namespace Modelowanie
+{
+    class Database
+    {
+        public static string connectionString = "Data Source=DESKTOP-6NU2EAE;Initial Catalog=uniterm;Integrated security=true";
+        private SqlConnection conString;
+
+        public Database() {
+            this.conString = new SqlConnection(Database.connectionString);
+        }
+
+        public Database(string conStr)
+        {
+            this.conString = new SqlConnection(conStr);
+        }
+
+
+        public SqlConnection ConnectionString
+        {
+            get
+            {
+                return this.conString;
+            }
+            set
+            {
+                this.conString = value;
+            }
+        }
+
+
+        public void Connect()
+        {
+            try
+            {
+                this.conString.Open();
+            }
+            catch (InvalidCastException ex)
+            {
+                throw ex;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                this.conString.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        public SqlDataAdapter CreateAdapter(string query) {
+            return new SqlDataAdapter(query, this.ConnectionString);
+        }
+
+        public DataTable CreateDataTable(string query)
+        {
+            DataTable tab = new DataTable();
+            this.Connect();
+
+            if (this.ConnectionString.State == ConnectionState.Open)
+            {
+                SqlDataAdapter ad = CreateAdapter(query);
+                ad.Fill(tab);
+            } else
+            {
+                throw new Exception("Nie mozna polaczyc z baza");
+            }
+
+            this.Disconnect();
+
+            return tab;
+        }
+
+        public DataRow CreateDataRow(string query)
+        {
+            DataTable tab = new DataTable();
+
+            DataRow row;
+
+            this.Connect();
+
+            if (this.ConnectionString.State == ConnectionState.Open)
+            {
+
+                SqlDataAdapter ad = CreateAdapter(query);
+
+                ad.Fill(tab);
+                try
+                {
+                    row = tab.Rows[0];
+                }
+                catch
+                {
+                    row = null;
+                }
+            }
+            else
+            {
+                throw new Exception("Nie można połączyć się z bazą daych");
+            }
+
+            this.Disconnect();
+
+            return row;
+        }
+
+        public void RunQuery(string query)
+        {
+            this.Connect();
+
+            if (this.ConnectionString.State == ConnectionState.Open)
+            {
+                SqlCommand cmd = new SqlCommand(query, this.ConnectionString);
+
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                throw new Exception("Nie można połączyć się z bazą daych");
+            }
+
+            this.Disconnect();
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Windows.Markup;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+
+namespace Modelowanie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Database db;
+        bool nowy = false, modified = false;
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            cDrawing.ClearAll();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (FontFamily f in System.Windows.Media.Fonts.SystemFontFamilies)
+            {
+                cbFonts.Items.Add(f);
+            }
+            if (cbFonts.Items.Count > 0)
+                cbFonts.SelectedIndex = 0;
+
+            for (int i = 8; i <= 40; i++)
+            {
+                cbfSize.Items.Add(i);
+            }
+            cbfSize.SelectedIndex = 4;
+
+          
+        }
+
+
+        private void ehCBFontsChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                UDrawing.fontFamily = new FontFamily(e.AddedItems[0].ToString());
+                modified = true;
+                System.Console.WriteLine("Hello world!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void ehcbfSizeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                UDrawing.fontsize = (int)e.AddedItems[0];
+                modified = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddUniterm au = new AddUniterm();
+
+            au.ShowDialog();
+
+            if (au.tbA.Text.Length > 250 || au.tbB.Text.Length > 250)
+            {
+                MessageBox.Show("Zbyt długi tekst!\n Maksymalna długość tekstu to 250 znaków!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            UDrawing.sA = au.tbA.Text;
+            UDrawing.sB = au.tbB.Text;
+
+            UDrawing.sOp = au.rbSr.IsChecked == true ? " ; " : " , ";
+
+            //btnRedraw_Click(sender, e);
+
+            modified = true;
+
+        }
+
+
+    }
+}
